22781c862454f7c495c0a08d15fbda6e1f02c1f6
Chaithra K C <chaithra.kc22@gmail.com>
2021-09-08 21:45:45 -0400

    shifting letters implementation

---
+++ strings/shifting_letters.py
@@ -0,0 +1,27 @@
+# Difficulty : Medium
+
+from typing import List
+
+
+class Solution(object):
+    def shifting_letters(self, s: str, shifts: List) -> str:
+        """
+        :type s: str
+        :type shifts: List[int]
+        :rtype: str
+        """
+        ans = list()
+        total_shifts = sum(shifts) % 26  # circular shift: if num of shifts goes beyond 27 (number of english alphabets)
+        for index, letter in enumerate(s):
+            alpha_index = ord(letter) - ord('a')
+            ascii_val = chr(ord('a') + (alpha_index + total_shifts) % 26)
+            ans.append(ascii_val)
+            total_shifts = (total_shifts - shifts[index]) % 26  # curcular shift
+
+        return ''.join(ans)
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    result = solution.shifting_letters("ruu", [26, 9, 17])
+    print(result)

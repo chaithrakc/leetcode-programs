2a1b064d02de0cede0737a696e8ac88902e4f648
Chaithra K C <chaithra.kc22@gmail.com>
2021-09-11 20:01:27 -0400

    Subtree of Another tree - reformatting

--- easy/p572SubtreeofAnotherTree.py
+++ easy/p572SubtreeofAnotherTree.py
@@ -5,14 +5,18 @@ from commons.BinaryTree import TreeNode
 
 class SolutionSubtree:
     def is_subtree(self, root: Optional[TreeNode], subroot: Optional[TreeNode]) -> bool:
-        if not root: return False
-        if self.isidentical(root, subroot): return True
+        if not root:
+            return False
+        if self.isidentical(root, subroot):
+            return True
         return self.is_subtree(root.left, subroot) or self.is_subtree(root.right, subroot)
 
     def isidentical(self, tree: TreeNode, subtree: TreeNode):
-        if not tree and not subtree: return True
-        if not tree or not subtree: return False
-        if tree.val != subtree.val: return False
-        return tree.val == subtree.val \
-               and self.isidentical(tree.left, subtree.left) \
-               and self.isidentical(tree.right, subtree.right)
+        if not tree and not subtree:
+            return True
+        if not tree or not subtree:
+            return False
+        if tree.val != subtree.val:
+            return False
+        return tree.val == subtree.val and self.isidentical(tree.left, subtree.left) and self.isidentical(tree.right,
+                                                                                                          subtree.right)

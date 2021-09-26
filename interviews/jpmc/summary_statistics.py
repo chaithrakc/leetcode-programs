fa7cbdadabd7df2fd5bb9c1ed1fc5e88661bbba6
Chaithra K C <chaithra.kc22@gmail.com>
2021-09-25 20:15:43 -0400

    interview experience

--- interviews/jpmc/summary_statistics.py
+++ interviews/jpmc/summary_statistics.py
@@ -1,9 +1,8 @@
-import statistics
 import numpy as np
 
 if __name__ == '__main__':
     lines = 'date,price,region\n2015-07-19,1.08,Louisville\n2015-08-16,1.05,RichmondNorfolk\n2015-04-05,1.1,' \
-            'Orlando\n2015-07-26,1.12,GrandRapids\n2015-05-31,1.1,Atlanta '
+            'Orlando\n2015-07-26,1.12,GrandRapids\n2015-05-31,1.1,Atlanta'
     lines = str.split(lines)
     prices = list()
     for line in lines:
@@ -14,4 +13,4 @@ if __name__ == '__main__':
     print('Min: %.2f' % np.min(prices))
     print('Max: %.2f' % np.max(prices))
     print('Mean: %.3f' % np.mean(prices))
-    print('Std: %.4f' % statistics.stdev(prices))
+    print('Std: %.4f' % np.std(prices, ddof=1))  # standard deciation based on a sample

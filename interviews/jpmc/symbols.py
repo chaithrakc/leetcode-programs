0b9369e14ae14860035841b5fec30444a92294b6
Chaithra K C <chaithra.kc22@gmail.com>
2022-01-23 15:51:45 -0500

    removing unwanted code

--- interviews/jpmc/symbols.py
+++
@@ -1,9 +0,0 @@
-if __name__ == '__main__':
-    line = '35 40'
-    nums = line.split()
-    if nums[0] > nums[1]:
-        print('>')
-    elif nums[0] < nums[1]:
-        print('<')
-    else:
-        print('=')

ca82477e436d7288206b7cba326342de772e4e52
Chaithra K C <chaithra.kc22@gmail.com>
2022-12-23 03:01:56 -0500

    update maxunits truck

--- easy/p1710MaximumUnitsTruck.py
+++ easy/p1710MaximumUnitsTruck.py
@@ -3,4 +3,7 @@ from typing import List
 
 class SolutionMaximumUnitsTruck:
     def maximumUnits(self, box_types: List[List[int]], truck_size: int) -> int:
-        pass
+        box_types.sort(key=lambda x: x[1], reverse=True)
+        for box in box_types:
+            pass
+

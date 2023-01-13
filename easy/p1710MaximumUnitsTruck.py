3ef1549d2c21e2c04bd43ef288ca7973866d4659
Chaithra K C <chaithra.kc22@gmail.com>
2023-01-12 22:18:19 -0500

    brute force solution

--- easy/p1710MaximumUnitsTruck.py
+++ easy/p1710MaximumUnitsTruck.py
@@ -2,8 +2,30 @@ from typing import List
 
 
 class SolutionMaximumUnitsTruck:
-    def maximumUnits(self, box_types: List[List[int]], truck_size: int) -> int:
+    def maximum_units(self, box_types: List[List[int]], truck_size: int) -> int:
+
+        # sorting in based on units per box in descending order
         box_types.sort(key=lambda x: x[1], reverse=True)
-        for box in box_types:
-            pass
+
+        boxes_loaded = 0
+        max_units = 0
+
+        # iterating over each box
+        for box_type in box_types:
+            num_boxes = box_type[0]
+            num_units_per_box = box_type[1]
+            # loading boxes onto truck
+            for box in range(num_boxes):
+                if boxes_loaded >= truck_size:  # number of boxes on the truck should not exceed truckSize
+                    return max_units
+                boxes_loaded = boxes_loaded + 1
+                max_units = max_units + num_units_per_box
+
+        # use case: number of boxes provided are less than truck size
+        return max_units
+
+
+
+
+
 
--- test/easy/test_p1710MaximumUnitsTruck.py
+++ test/easy/test_p1710MaximumUnitsTruck.py
@@ -7,6 +7,7 @@ from easy.p1710MaximumUnitsTruck import SolutionMaximumUnitsTruck
 max_units_truck = [
     ([[1, 3], [2, 2], [3, 1]], 4, 8),
     ([[5, 10], [2, 5], [4, 7], [3, 9]], 10, 91),
+    ([[1, 3], [5, 5], [2, 5], [4, 2], [4, 1], [3, 1], [2, 2], [1, 3], [2, 5], [3, 2]], 35, 76)
 ]
 
 
@@ -15,4 +16,4 @@ class TestSolutionMaximumUnitsTruck:
 
     @pytest.mark.parametrize('box_types, truck_size, max_units', max_units_truck)
     def test_maximum_units(self, box_types: List[List[int]], truck_size: int, max_units: int):
-        assert self.solution.maximumUnits(box_types, truck_size) == max_units
+        assert self.solution.maximum_units(box_types, truck_size) == max_units

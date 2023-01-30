c7fe3a62082bd5f84e9cd1b57fa80f36bf634843
Chaithra K C <chaithra.kc22@gmail.com>
2023-01-30 12:30:43 -0500

    finding palindromes

---
+++ misc/palindromes.py
@@ -0,0 +1,26 @@
+# https://techbeamers.com/find-palindromes-anagrams-list-strings-python/
+
+"""
+Program:
+ Python program to find palindromes in a list of strings using Python lambda.
+
+"""
+from typing import List
+
+def get_palindromes(input_strings:List[str]):
+    palindromes_slicing = list(filter(lambda the_str: the_str.lower() == the_str.lower()[::-1], input_strings))
+    palindromes_reverse_func = list(filter(lambda the_str: the_str.lower() == "".join(reversed(the_str.lower())),
+                                           input_strings))
+
+    print("*******************")
+    print('Palindrome with string slicing = {}'.format(palindromes_slicing))
+    print('Palindromes with reverse function= {}'.format(palindromes_reverse_func))
+    print("*******************\n")
+
+def driver():
+    input_string_list = ['Python', 'Radar', 'CSharp', 'Madam', 'Programmer', 'Noon', 'Refer', 'Php', 'Go']
+    get_palindromes(input_string_list)
+
+
+if __name__ == '__main__':
+    driver() # call driver function

25347111a562cc1a448f5282e633825f09d18588
Chaithra K C <chaithra.kc22@gmail.com>
2023-02-04 20:01:05 -0500

    updating key-value pair to dict

--- misc/insert_dict_kv.py
+++ misc/insert_dict_kv.py
@@ -8,4 +8,5 @@ aValue = 500
 aDict = {10:100, 15:120, 16:200}
 
 aDict.update({aKey:aValue}) # it updates the value if key is already present
+aDict.update({'Chaithra':'Casey'}) # adds the key-value pair if key is not present
 print(aDict)

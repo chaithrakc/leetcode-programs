5266668c3c05875e47b3f6500bac1ac385a05913
Chaithra K C <chaithra.kc22@gmail.com>
2023-02-04 20:11:38 -0500

    formatting

--- misc/list_to_dict.py
+++ misc/list_to_dict.py
@@ -2,8 +2,8 @@
 # combine the two lists using zip() method
 #  then, convert zip object into dictionary using dict() constructor method
 
-list_of_keys = [1,2,3,4,5]
-list_of_values = [11,22,33,44,55]
+list_of_keys = [1, 2, 3, 4, 5]
+list_of_values = [11, 22, 33, 44, 55]
 
 the_dict = dict(zip(list_of_keys, list_of_values))
 print(the_dict)

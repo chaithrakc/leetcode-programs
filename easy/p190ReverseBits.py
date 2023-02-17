f6cc74f1542dc60b0772b0d60dae01b91bf07c9e
Chaithra K C <chaithra.kc22@gmail.com>
2023-02-16 20:49:51 -0500

    updating the comments

--- easy/p190ReverseBits.py
+++ easy/p190ReverseBits.py
@@ -2,8 +2,8 @@ class SolutionReverseBits:
     def reverseBits(self, n: int) -> int:
         res = 0
         for i in range(32):
-            bit = (n >> i) & 1  # extract the bits from start to end (0-31) by right shifting and &
-            res = res | (bit << (31-i))  # insert the bits from end to start by left shifting and |
+            bit = (n >> i) & 1  # extract the bits from end to start (31 - 0) by right shifting and & operation
+            res = res | (bit << (31-i))  # insert the bits from start to end by left shifting and |
 
             # or operation is needed so that previous bits that are already should not be changed
         return res

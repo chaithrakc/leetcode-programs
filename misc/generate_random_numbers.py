78f78d43876c78f61b3c5ae8432fba64478079b7
Chaithra K C <chaithra.kc22@gmail.com>
2023-03-29 18:25:17 -0400

    updating the code

--- misc/generate_random_numbers.py
+++ misc/generate_random_numbers.py
@@ -1,4 +1,7 @@
 import random as rand
+import numpy as np
 
 for i in range(11):
     print(rand.randint(0, 500)) # generates random number between 0 and 500
+
+print('numpy random nums: {}'.format(np.random.randint(low=0, high=500, size=10)))

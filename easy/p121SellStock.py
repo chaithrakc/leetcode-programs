d9057acfade502718402db910d60f581180be66e
Chaithra K C <chaithra.kc22@gmail.com>
2023-03-31 10:45:43 -0400

    updating the code

--- easy/p121SellStock.py
+++ easy/p121SellStock.py
@@ -9,11 +9,12 @@ class SolutionSellStock:
     # Space Complexity: O(1)
     def max_profit(self, prices: List[int]) -> int:
         max_profit = 0
-        buy_index = 0
-        for sell_index in range(1, len(prices)):
-            if prices[sell_index] < prices[buy_index]:
-                buy_index = sell_index  # buy index should always point to min price index
+        left_ptr = 0 # buy low; should point to the lowest value at any point
+        # right_ptr (sell high ; should point to the highest value at any point)
+        for right_ptr in range(1, len(prices)):
+            if prices[right_ptr] < prices[left_ptr]:
+                left_ptr = right_ptr  # buy index should always point to min price index
             else:
-                profit = prices[sell_index] - prices[buy_index]
+                profit = prices[right_ptr] - prices[left_ptr]
                 max_profit = max(profit, max_profit)
         return max_profit
